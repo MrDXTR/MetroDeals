17ad9da16241625c8eeea77fd7dc29ce20883dde
MrDXTR <manav.chillar20@st.niituniversity.in>
2023-05-12 03:45:36 +0530

    minor UI changes

--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -50,7 +50,7 @@ const Navbar = () => {
             <Button variant="contained" className={classes.logout} color="secondary" onClick={logout}>Logout</Button>
           </div>
         ) : (
-          <Button component={Link} to="/auth" variant="contained" color="primary">Sign In</Button>
+          <Button component={Link} to="/auth" variant="contained" color="primary"className={classes.signInButton}>Sign In</Button>
         )}
       </Toolbar>
     </AppBar>
