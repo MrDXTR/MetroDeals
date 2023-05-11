4ed7697befac80ca135904e20143fc2b6d474f1e
jrap-bit <namanpandey10@gmail.com>
2023-05-12 01:33:02 +0530

    Changed Model for better data collection

--- server/index.js
+++ server/index.js
@@ -16,7 +16,7 @@ app.use(cors());
 app.use('/posts', postRoutes);
 app.use("/user", userRouter);
 
-const CONNECTION_URL = 'mongodb+srv://js_mastery:M6WfDnJEoj9HkV2d@practice.jto9p.mongodb.net/memories_app?retryWrites=true&w=majority';
+const CONNECTION_URL = 'mongodb://localhost:27017/metro-deals';
 const PORT = process.env.PORT|| 5000;
 
 mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
--- server/models/user.js
+++ server/models/user.js
@@ -5,6 +5,9 @@ const userSchema = mongoose.Schema({
   email: { type: String, required: true },
   password: { type: String, required: true },
   id: { type: String },
+  isAdmin: { type: Boolean, default: false, required:true },
+  loginTime: { type: Date, default: Date.now()},
+  loginType: { type: String, required: true },
 });
 
 export default mongoose.model("User", userSchema);
